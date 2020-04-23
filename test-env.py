b1d8509ee1f604c058e3787e679eae65f570f8b4
Noah Sadaka <noahsadaka@gmail.com>
2020-04-23 15:12:28 -0400

    change section scale

--- test-env.py
+++ test-env.py
@@ -16,6 +16,7 @@ from ceasiompy.utils.cpacsfunctions import aircraft_name, open_tixi, close_tixi,
 ## send to geometry_eval to get length and number of segments
 ## Calculate the scale factor from the total length
 ## Get the scaling for each section using xpath and tixiGetDoubleElement
+## get number of fuselage positionings
 ## Get the length for each fuselage positioning
 ## Rewrite the section scaling by rescaling with scale factor
 ## Rewrite the fuselage positioning length by rescaling with scale factor
@@ -44,15 +45,39 @@ def transformer(input_file, output_file='output_cpacs.xml', geometry_dict={}):
     scale = fuse_length_change/fuse_length
 
     tixi_handle = open_tixi(input_file)
-    fuselage_xpath = '/cpacs/vehicles/aircraft/model/fuselages/fuselage'
-    #uid = get_uid(tixi_handle, '/cpacs/vehicles/profiles/fuselageProfiles/fuselageProfile[1]')
-    test = tixi_handle.getDoubleElement(fuselage_xpath+'/positionings/positioning[2]/length')
-    return test
+    #fuselage_xpath = '/cpacs/vehicles/aircraft/model/fuselages/fuselage'
+    #test = tixi_handle.getDoubleElement(fuselage_xpath+'/positionings/positioning[2]/length')
+    tixi_handle = section_transformer(tixi_handle, scale, ag.fuse_sec_nb[0])
+    return tixi_handle
 
+def section_transformer(tixi_handle, scale, num_sec):
+    sections_xpath = '/cpacs/vehicles/aircraft/model/fuselages/fuselage/sections/'
+    for i in np.arange(num_sec):
+        section_xpath = sections_xpath + f'section[{i+1}]/transformation/scaling/'
+        # get current values
+        x_val = tixi_handle.getDoubleElement(section_xpath+'x')
+        y_val = tixi_handle.getDoubleElement(section_xpath+'y')
+        z_val = tixi_handle.getDoubleElement(section_xpath+'z')
+        # update current values
+        tixi_handle.updateDoubleElement(section_xpath+'x', x_val*scale, '%.5f')
+        tixi_handle.updateDoubleElement(section_xpath+'y', y_val*scale, '%.5f')
+        tixi_handle.updateDoubleElement(section_xpath+'z', z_val*scale, '%.5f')
+
+    return tixi_handle
+
+
+
+
+#------------------------------
+# MAIN
+#------------------------------
+# for testing purposes, delete later
 if os.path.exists('cpacs/test_cpacs.xml'):
     os.remove('cpacs/test_cpacs.xml')
 os.system('cp cpacs/original/test_cpacs.xml cpacs/test_cpacs.xml')
 
-print(transformer(input_file='cpacs/test_cpacs.xml', geometry_dict={'fuse_length':10}))
+tixi_handle = transformer(input_file='cpacs/test_cpacs.xml', geometry_dict={'fuse_length':10})
+
+print(tixi_handle.getDoubleElement('/cpacs/vehicles/aircraft/model/fuselages/fuselage/sections/section[1]/transformation/scaling/x'))
 
 
