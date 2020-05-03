8be47feab8627cfb34d455b6fb0f1ebf1c5aa830
Noah Sadaka <noahsadaka@gmail.com>
2020-05-03 14:10:18 -0400

    profile creation function

--- simplifiedgeometry.py
+++ simplifiedgeometry.py
@@ -132,13 +132,13 @@ def cpacs_generate(aircraftname, tot_len, nose_frac=0.1, tail_frac=0.1):
     tixi.Tixi3.create(tixi_handle, rootElementName='cpacs')
 
     # Define schema and fill in header
-    tixi_handle.declareNamespace("/cpacs", "http://www.w3.org/2001/XMLSchema-instance", "xsi");
+    tixi_handle.declareNamespace("/cpacs", "http://www.w3.org/2001/XMLSchema-instance", "xsi")
 
-    tixi_handle.registerNamespace("http://www.w3.org/2001/XMLSchema-instance", "xsi");
+    tixi_handle.registerNamespace("http://www.w3.org/2001/XMLSchema-instance", "xsi")
 
-    tixi_handle.addTextAttribute("/cpacs", "xsi:noNamespaceSchemaLocation", "cpacs_schema.xsd");
+    tixi_handle.addTextAttribute("/cpacs", "xsi:noNamespaceSchemaLocation", "cpacs_schema.xsd")
 
-    tixi_handle.addCpacsHeader(name=aircraftname, creator='Noah Sadaka', version='N/A', description='...', cpacsVersion='3.2');
+    tixi_handle.addCpacsHeader(name=aircraftname, creator='Noah Sadaka', version='N/A', description='...', cpacsVersion='3.2')
 
     # Create XML elements down to sections
     path_elements = ['vehicles', 'aircraft', 'model', 'fuselages', 'fuselage', 'sections']
@@ -190,12 +190,54 @@ def cpacs_generate(aircraftname, tot_len, nose_frac=0.1, tail_frac=0.1):
                                                         'z', 1, '%d')
         base_path += f"/{i}"
 
+    profile_id, tixi_handle = add_circular_fuse_profile(tixi_handle)
+
     # Check that CPACS file matches schema
     #tixi_handle.schemaValidateFromFile('cpacs_schema.xsd')
     close_tixi(tixi_handle, 'cpacs/test_fuse.xml')
 
+def add_circular_fuse_profile(tixi_handle):
+    """Adds a circular profile to the CPACS file
 
-def add_section(name, z-coord)
+    Parameters
+    ----------
+    tixi_handle : tixi handle object
+        A tixi handle to the cpacs file to be created
+
+    Returns
+    -------
+    tixi_handle : tixi handle object
+    profile_id : str
+        uID of the profile,
+    """
+
+    base_path = '/cpacs/vehicles'
+    tixi_handle.createElement(base_path, 'profiles')
+    base_path += '/profiles'
+    tixi_handle.createElement(base_path, 'fuselageProfile')
+    base_path+= '/fuselageProfile'
+    profile_id = 'fuselageCircleProfileID'
+    add_uid(tixi_handle, base_path, profile_id)
+    tixi_handle.addTextElement(base_path, 'name', 'Circle')
+    tixi_handle.addTextElement(base_path, 'description', 'Profile build up from set of points on circle where dimensions are 1 ... -1')
+    tixi_handle.createElement(base_path, 'pointList')
+    base_path += '/pointList'
+    
+    # Add points
+    x_vec = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    y_vec = [0.0,0.0774924206719,0.154518792808,0.230615870742,0.305325997695,0.378199858172,0.4487991802,0.516699371152,0.581492071288,0.642787609687,0.700217347767,0.753435896328,0.802123192755,0.84598642592,0.884761797177,0.91821610688,0.946148156876,0.968389960528,0.984807753012,0.995302795793,0.999811970449,0.998308158271,0.990800403365,0.977333858251,0.957989512315,0.932883704732,0.902167424781,0.866025403784,0.824675004109,0.778364911924,0.727373641573,0.672007860556,0.612600545193,0.549508978071,0.483112599297,0.413810724505,0.342020143326,0.268172612761,0.192712260548,0.116092914125,0.0387753712568,-0.0387753712568,-0.116092914125,-0.192712260548,-0.268172612761,-0.342020143326,-0.413810724505,-0.483112599297,-0.549508978071,-0.612600545193,-0.672007860556,-0.727373641573,-0.778364911924,-0.824675004109,-0.866025403784,-0.902167424781,-0.932883704732,-0.957989512315,-0.977333858251,-0.990800403365,-0.998308158271,-0.999811970449,-0.995302795793,-0.984807753012,-0.968389960528,-0.946148156876,-0.91821610688,-0.884761797177,-0.84598642592,-0.802123192755,-0.753435896328,-0.700217347767,-0.642787609687,-0.581492071288,-0.516699371152,-0.4487991802,-0.378199858172,-0.305325997695,-0.230615870742,-0.154518792808,-0.0774924206719,0.0]
+    z_vec = [1.0,0.996992941168,0.987989849477,0.97304487058,0.952247885338,0.925723969269,0.893632640323,0.85616689953,0.813552070263,0.766044443119,0.713929734558,0.657521368569,0.597158591703,0.533204432802,0.466043519703,0.396079766039,0.323733942058,0.249441144058,0.173648177667,0.0968108707032,0.0193913317718,-0.0581448289105,-0.13533129975,-0.211703872229,-0.286803232711,-0.360177724805,-0.431386065681,-0.5,-0.565606875487,-0.627812124672,-0.686241637869,-0.740544013109,-0.790392669519,-0.835487811413,-0.875558231302,-0.910362940966,-0.939692620786,-0.963370878616,-0.981255310627,-0.993238357742,-0.999247952504,-0.999247952504,-0.993238357742,-0.981255310627,-0.963370878616,-0.939692620786,-0.910362940966,-0.875558231302,-0.835487811413,-0.790392669519,-0.740544013109,-0.686241637869,-0.627812124672,-0.565606875487,-0.5,-0.431386065681,-0.360177724805,-0.286803232711,-0.211703872229,-0.13533129975,-0.0581448289105,0.0193913317718,0.0968108707032,0.173648177667,0.249441144058,0.323733942058,0.396079766039,0.466043519703,0.533204432802,0.597158591703,0.657521368569,0.713929734558,0.766044443119,0.813552070263,0.85616689953,0.893632640323,0.925723969269,0.952247885338,0.97304487058,0.987989849477,0.996992941168,1.0]
+    tixi_handle.addFloatVector(base_path, 'x', x_vec, len(x_vec), '%g')
+    tixi_handle.addFloatVector(base_path, 'y', y_vec, len(y_vec), '%g')
+    tixi_handle.addFloatVector(base_path, 'z', z_vec, len(z_vec), '%g')
+
+
+
+    return profile_id, tixi_handle
+
+
+
+#def add_section(name, z-coord, profile_id)
 
 
 # ------------------------------
